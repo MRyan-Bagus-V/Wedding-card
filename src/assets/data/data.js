5b06183414290c0e04522f963dbd5d85d8315a76
MRyan Bagus Valentine <m.ryanbagusv@gmail.com>
2025-07-23 00:54:38 +0700

    first commit

---
+++ src/assets/data/data.js
@@ -0,0 +1,126 @@
+export const data = {
+    bride: {
+        L: {
+            id: 1,
+            name: 'M Ryan Bagus Valentin, S.Kom',
+            child: 'Putra Kedua',
+            father: 'Ahmad Dedy Surya Irwandi, Alm',
+            mother: 'Dadah Siti Saadah',
+            image: './src/assets/images/cowo.png'
+        },
+        P: {
+            id: 2,
+            name: 'Lila Pratama Putri, S.Keb',
+            child: 'Putri Pertama',
+            father: 'Ermanto',
+            mother: 'Susilawati',
+            image: './src/assets/images/cewe.png'
+        },
+
+        couple: './src/assets/images/duo.jpg'
+    },
+
+    time: {
+        marriage: {
+            year: '2025',
+            month: 'September',
+            date: '06',
+            day: 'Sabtu',
+            hours: {
+                start: '08.00',
+                finish: 'Selesai'
+            }
+        },
+        reception: {
+            year: '2025',
+            month: 'September',
+            date: '06',
+            day: 'Sabtu',
+            hours: {
+                start: '11.00',
+                finish: 'Selesai'
+            }
+        },
+        address: 'Kp. Lamping, RT 03/ RW 05, Margalaksana, Kecamatan Cilawu, Kabupaten Garut'
+    },
+
+    link: {
+        calendar: 'https://calendar.app.google/oSVLRMYC79GzuA4f9',
+        map: 'https://maps.app.goo.gl/q1Ask2Jgd4ekiiKBA',
+    },
+
+    galeri: [
+        {
+            id: 1,
+            image: './src/assets/images/1.png'
+        },
+        {
+            id: 2,
+            image: './src/assets/images/2.png'
+        },
+        {
+            id: 3,
+            image: './src/assets/images/3.png'
+        },
+        {
+            id: 4,
+            image: './src/assets/images/4.png'
+        },
+        {
+            id: 5,
+            image: './src/assets/images/5.png'
+        }
+    ],
+
+    bank: [
+        {
+            id: 1,
+            name: 'M Ryan Bagus Valentin',
+            icon: './src/assets/images/bca.png',
+            rekening: '12345678'
+        },
+        {
+            id: 2,
+            name: 'Lila Pratama Putri',
+            icon: './src/assets/images/bri.png',
+            rekening: '12345678'
+        },
+    ],
+
+    audio: './src/assets/audio/pure-love-304010.mp3',
+
+    api: 'https://script.google.com/macros/s/AKfycbyydz6N4p2VWUG8zsXeURv6ap9RP8a4eC3x6N3x6qTDjMVr1cIBz9S0NsHw2rWvBOSXGg/exec',
+
+    navbar: [
+        {
+            id: 1,
+            teks: 'Home',
+            icon: 'bx bxs-home-heart',
+            path: '#home',
+        },
+        {
+            id: 2,
+            teks: 'Mempelai',
+            icon: 'bx bxs-group',
+            path: '#bride',
+        },
+        {
+            id: 3,
+            teks: 'Tanggal',
+            icon: 'bx bxs-calendar-check',
+            path: '#time',
+        },
+        {
+            id: 4,
+            teks: 'Galeri',
+            icon: 'bx bxs-photo-album',
+            path: '#galeri',
+        },
+        {
+            id: 5,
+            teks: 'Ucapan',
+            icon: 'bx bxs-message-rounded-dots',
+            path: '#wishas',
+        },
+    ],
+}
