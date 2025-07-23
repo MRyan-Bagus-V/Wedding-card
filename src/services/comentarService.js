2e9978a2e57045777fd32584c2393eb068daaed0
MRyan Bagus Valentine <m.ryanbagusv@gmail.com>
2025-07-23 09:00:28 +0700

    update api

--- src/services/comentarService.js
+++ src/services/comentarService.js
@@ -10,37 +10,31 @@ export const comentarService = {
         }
     },
 
-addComentar: async function ({id, name, status, message, date, color}) {
-    const comentar = {
-        id,
-        name,
-        status,
-        message,
-        date,
-        color,
-    };
+    addComentar: async function ({id, name, status, message, date, color}) {
+        const comentar = {
+            id: id,
+            name: name,
+            status: status,
+            message: message,
+            date: date,
+            color: color,
+        };
 
-    try {
-        const response = await fetch(data.api, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(comentar),
-        });
+        try {
+            const response = await fetch(data.api, {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(comentar),
+            });
 
-        // Cek apakah response benar-benar berisi JSON
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.includes("application/json")) {
             return await response.json();
-        } else {
-            const text = await response.text();
-            return { error: "Response bukan JSON", raw: text };
-        }
 
-    } catch (error) {
-        console.error('Post error:', error);
-        return { error: error.message };
-    }
-}
+        } catch (error) {
+            console.error('Post error:', error);
+            return {error: error.message};
+        }
+    },
 };
